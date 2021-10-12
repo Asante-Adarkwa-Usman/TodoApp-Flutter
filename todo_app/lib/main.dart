3691200cb6315b3cd3a61d221d50c0268335e1b6
Asante Adarkwa Usman <asante.au96@gmail.com>
2021-10-12 12:08:08 -0700

    Done with UI and theming

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/views/home_view.dart';
 
+import 'utils/utils.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -12,9 +14,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Todo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        shadowColor: Colors.white38,
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: const Color.fromRGBO(249, 251, 255, 1),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: customBlue,
+            fontSize: 21.0,
+            fontWeight: FontWeight.w600,
+          ),
+          actionsIconTheme: IconThemeData(
+            color: customBlue,
+          ),
+        ),
       ),
-      darkTheme: ThemeData.dark(),
+      darkTheme: ThemeData.dark().copyWith(
+        shadowColor: Colors.black38,
+        primaryColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: customBlue,
+            fontSize: 21.0,
+            fontWeight: FontWeight.w600,
+          ),
+          actionsIconTheme: IconThemeData(
+            color: customBlue,
+          ),
+        ),
+      ),
       themeMode: ThemeMode.system,
       home: const HomeView(),
     );
--- todo_app/lib/views/home_view.dart
+++ todo_app/lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/utils/utils.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -6,24 +7,26 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          leadingWidth: 40.0,
-          title: const Text('My Tasks'),
-          leading: const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              radius: 45.0,
-              backgroundColor: Colors.white,
-              backgroundImage: AssetImage('assets/profile.jpeg'),
-            ),
+      appBar: AppBar(
+        centerTitle: false,
+        leadingWidth: 50.0,
+        title: const Text('My Tasks'),
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 45.0,
+            backgroundColor: Colors.white,
+            backgroundImage: AssetImage('assets/profile.jpeg'),
           ),
-          actions: <Widget>[
-            IconButton(onPressed: () {}, icon: const Icon(Icons.sort)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-          ],
         ),
-        body: ListView.separated(
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: const Icon(Icons.sort)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
           padding: const EdgeInsets.all(15.0),
           itemBuilder: (BuildContext context, index) {
             return const TodoTileWidget();
@@ -34,7 +37,61 @@ class HomeView extends StatelessWidget {
             );
           },
           itemCount: 10,
-        ));
+        ),
+      ),
+      floatingActionButton:
+          const FloatingActionButton(onPressed: null, child: Icon(Icons.add)),
+      bottomNavigationBar: SafeArea(
+        child: InkWell(
+          onTap: () {
+            showModalBottomSheet(
+                context: context,
+                // shape:
+                builder: (context) {
+                  return ListView(
+                    children: [],
+                  );
+                });
+          },
+          child: Container(
+              height: 50.0,
+              margin: const EdgeInsets.all(16),
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              // color: Colors.blue,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const Icon(Icons.check_circle, color: customBlue),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      Text('Completed',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  color: customBlue)),
+                      const Icon(Icons.keyboard_arrow_down, color: customBlue)
+                    ],
+                  ),
+                  Text('24',
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText2!
+                          .copyWith(color: customBlue)),
+                ],
+              )),
+        ),
+      ),
+    );
   }
 }
 
@@ -47,16 +104,25 @@ class TodoTileWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        leading:
-            const Icon(Icons.check_circle_outline_rounded, color: Colors.pink),
-        title: const Text("Plan trip to Finland"),
-        subtitle: const Text("Plan for the family's trip next summer"),
+        leading: Icon(Icons.check_circle_outline_rounded,
+            size: 30, color: dateColor('Today')),
+        title: Text("Plan trip to Finland",
+            style:
+                Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 20)),
+        subtitle: Text("Plan for the family's trip next summer",
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2!
+                .copyWith(color: Colors.grey)),
         trailing: TextButton.icon(
           onPressed: () {},
-          icon: const Icon(Icons.notifications, color: Colors.pink),
-          label: const Text(
+          icon: Icon(Icons.notifications, color: dateColor('Yesterday')),
+          label: Text(
             "Yesterday",
-            style: TextStyle(color: Colors.pink),
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2!
+                .copyWith(color: dateColor('Yesterday')),
           ),
         ),
       ),
